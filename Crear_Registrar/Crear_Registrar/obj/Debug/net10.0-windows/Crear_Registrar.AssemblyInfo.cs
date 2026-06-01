84ef0fd52958b7532bbaf3208956f9c1b27beb3a
Xari <31975280+deerfoster@users.noreply.github.com>
2026-05-31 22:47:10 -0400

    update

--- Crear_Registrar/Crear_Registrar/obj/Debug/net10.0-windows/Crear_Registrar.AssemblyInfo.cs
+++ Crear_Registrar/Crear_Registrar/obj/Debug/net10.0-windows/Crear_Registrar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crear_Registrar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcf279219e589b26036e811e88568aeee2b23b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crear_Registrar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crear_Registrar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
